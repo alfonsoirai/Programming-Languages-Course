0ca1506d256e24e1c9c675c7761b44fe22c481b9
alfonsoirai <pon.alfonsoirai@gmail.com>
2018-06-17 18:11:49 -0500

    Homework 3.3

--- logic_programming/homework3-3.pl
+++ logic_programming/homework3-3.pl
@@ -22,3 +22,36 @@ palindrome(List) :- rev(List, List).
 % Exercise 6.3
 
 toptail(InList, OutList):- append([_|OutList], [_], InList).
+
+% Exercise 6.4
+
+% 1
+lastr([], X).
+lastr(List, X) :- reverse(List, [X|_]).
+
+% 2
+lastrec([X], X).
+lastrec([_|T], X) :- lastrec(T, X).
+
+% Exercise 6.5
+
+swapfl([], []).
+swapfl([F1 | T1], [F2 | T2]) :- reverse(T1, [L1 | R1]), reverse(T2, [L2 | R2]),
+                                F1 = L2,
+                                L1 = F2,
+                                R1 = R2.
+
+% Exercise 6.6
+
+prefix(P, L) :- append(P, _, L).
+suffix(S, L) :- append(_, S, L).
+sublist(Sublist, L) :- suffix(S, L), prefix(Sublist, S).
+
+zebraowner(Zebraowner) :- Street = [_, _, _],
+                          member(house(red, enlishman, _), Street),
+                          member(house(_, spanish, jaguar), Street),
+                          member(house(_, Zebraowner, zebra), Street),
+                          sublist([house(_, _, snail), house(_, japanese, _)], Street),
+                          sublist([house(_, _, snail), house(blue, _, _)], Street).
+
+                
